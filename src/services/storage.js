e0ee8fd31b9329d29c7bb0df399409df184089cb
Elena <litishia@ya.ru>
2018-11-26 12:33:04 -0500

    made some prettyfing changed

--- src/services/storage.js
+++ src/services/storage.js
@@ -1,30 +1,29 @@
 import Time from "@/services/time.js";
 
 const storage = {
-  getItem: function(name, callback){
-    let item = JSON.parse( localStorage.getItem(name) );
-    if(item){
+  getItem: function(name, callback) {
+    let item = JSON.parse(localStorage.getItem(name));
+    if (item) {
       if (typeof callback === "function") {
         return callback(item);
       }
       return item;
-    };
-
+    }
     return this.defaultValues[name];
   },
   defaultValues: {
     savedCards: new Map(),
     lastID: 0
   },
-  getAllCards(){
+  getAllCards() {
     return [...this.savedCards.values()]; // get array from storage Map
   },
-  getNextID: function(){
+  getNextID: function() {
     this.lastID++;
     this.save("lastID", this.lastID);
     return this.lastID;
   },
-  createCard: function(cardData){
+  createCard: function(cardData) {
     cardData.id = this.getNextID();
     cardData.creationTime = Time.getDateTime();
     cardData.lastEditTime = cardData.creationTime;
@@ -32,21 +31,21 @@ const storage = {
     this.save("savedCards", [...this.savedCards]);
     return cardData;
   },
-  save: function(name, value){
-    localStorage.setItem( name, JSON.stringify(value) );
+  save: function(name, value) {
+    localStorage.setItem(name, JSON.stringify(value));
   },
-  delete: function(id){
+  delete: function(id) {
     this.savedCards.delete(id);
     this.save("savedCards", [...this.savedCards]);
   },
-  edit: function(editedData){
+  edit: function(editedData) {
     editedData.lastEditTime = Time.getDateTime();
     this.savedCards.set(editedData.id, editedData);
     this.save("savedCards", [...this.savedCards]);
   }
-}
+};
 storage.lastID = storage.getItem("lastID");
-storage.savedCards = storage.getItem("savedCards", function(arr){
+storage.savedCards = storage.getItem("savedCards", function(arr) {
   return new Map(arr);
 });
 export default storage;
--- src/services/time.js
+++ src/services/time.js
@@ -1,15 +1,15 @@
 const time = {
-  getDateTime: function(){
-    let currentTime = new Date;
+  getDateTime: function() {
+    let currentTime = new Date();
     let monthsEng = ['Jan','Feb','Mar','Apr','May','June','July','Aug','Sep','Oct','Nov','Dec'];
     let currentMonth = monthsEng[currentTime.getMonth()];
-    let currentHour = this.setZero( currentTime.getHours() );
-    let currentMins = this.setZero( currentTime.getMinutes() );
-    let eventTime = currentTime.getDate() +" "+ currentMonth + ", " + currentHour +":"+ currentMins;
+    let currentHour = this.setZero(currentTime.getHours());
+    let currentMins = this.setZero(currentTime.getMinutes());
+    let eventTime = currentTime.getDate() + " " + currentMonth + ", " + currentHour + ":" + currentMins;
     return eventTime;
   },
-  setZero: function(time){
-    if(time < 10){
+  setZero: function(time) {
+    if (time < 10) {
       time = "0" + time;
     }
     return time;
