018f4289b919037ea1a34556634ab18c04c90dad
Elena <litishia@ya.ru>
2018-11-25 20:48:30 -0500

    add 0 in front of single figures in time

--- src/services/time.js
+++ src/services/time.js
@@ -3,8 +3,16 @@ const time = {
     let currentTime = new Date;
     let monthsEng = ['Jan','Feb','Mar','Apr','May','June','July','Aug','Sep','Oct','Nov','Dec'];
     let currentMonth = monthsEng[currentTime.getMonth()];
-    let eventTime = currentTime.getDate() +" "+ currentMonth + ", " + currentTime.getHours() +":"+ currentTime.getMinutes();
+    let currentHour = this.setZero( currentTime.getHours() );
+    let currentMins = this.setZero( currentTime.getMinutes() );
+    let eventTime = currentTime.getDate() +" "+ currentMonth + ", " + currentHour +":"+ currentMins;
     return eventTime;
+  },
+  setZero: function(time){
+    if(time < 10){
+      time = "0" + time;
+    }
+    return time;
   }
 };
 export default time;
