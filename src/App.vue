3b827853874ab7dac7c00ef7e344d28d345f927e
Elena <litishia@ya.ru>
2018-12-03 13:17:12 -0500

    deleted default files

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,8 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/example">Example</router-link> |
-      <router-link to="/cards">Cards</router-link>
-    </div>
+    <!-- <div id="nav">
+    |  <router-link to="/cards">Cards</router-link> |
+    </div> -->
     <router-view/>
   </div>
 </template>
